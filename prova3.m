e2e7c95cc68077183ac1e3fe58dba46ebf9b60fe
lorenzobertella <lorenzo.bertella@studenti.unitn.it>
2022-08-25 09:14:21 +0200

    ultima versione

--- prova3.m
+++ prova3.m
@@ -20,26 +20,28 @@ q_f = [qA(1); qA(2)];
 dq_f  = [0; 0];
 ddq_f = [0; 0];
 
-q_o = [qA(1); qA(2)]; 
 % Frequenza del controllo:
-dt = 0.01;                                 %controllo a 100 Hz
+dt = 0.01;                          %controllo a 100 Hz
 
 % Guadagni regolatore:
-kp = 20;                                   %guadagno regolatore parte proporzionale
-kd = 2*sqrt(kp);                           %guadagno regolatore parte derivativa
+kp = 20;                            %guadagno regolatore parte proporzionale
+kd = 2*sqrt(kp);                    %guadagno regolatore parte derivativa
 
 % Variabili utili per il codice:
-nmax = 1500;                               %numero max cicli
-npos = 9;                                 %numero posizionamenti
-c = zeros;                                 %output boundary
+nmax = 1500;                        %numero max cicli (robot)
+npos = 1000;                        %numero max posizionamenti
+c = zeros;                          %output boundary
+q_o = [qA(1); qA(2)];               %variabile che memorizza la posizione
 
-% Plot ostacolo (cerchio):
-r = 0.7;                                   %raggio
-c_o = [1 3];                               %centro
-n_o = 1000;                                %precisione disegno
+% Definizione ostacolo (cerchio):
+r = 0.7;                            %raggio
+c_o = [1 3];                        %centro
+n_o = 1000;                         %precisione disegno
 t = linspace(0,2*pi,n_o);
 x_o = c_o(1) + r*sin(t);
 y_o = c_o(2) + r*cos(t);
+
+% Plot ostacolo:
 line(x_o,y_o)                              
 fill(x_o,y_o,'r')
 hold on
@@ -51,7 +53,7 @@ xlabel('q1')
 ylabel('q2')
 
 % Lista punti che si vogliono raggiungere:
-punti = [-2 1;-3 3;-1 3.5;-2 4;-1 2.5;0 3.75;2 3;0 2;-0.5 2.5];
+punti = [-2 1;-3 3;-1 3.5;-2 4;-1 2.5;0 3.75;2 3];
 
 % Ciclo:
 for pos = 1:npos
@@ -59,25 +61,34 @@ for pos = 1:npos
     z = 0;                                     
     % Inizializzo punto partenda entrambi robot
     qA = q_o;
+    q = qA;
     q_f = qA;
-    q = q_o;
+    
+    % Condizione per non eccedere dimensione vettore punti
+    if pos > length(punti)
+        break
+    end
+
     % Inizializzo punto arrivo entrambi robot
     qB = punti(pos,:)';
-
+    
     % Robot simulato:
-    e = qB - qA;                                        
-    de = -dq;
-    run_loop = 1;                                       %condizione cicli
-    i = 0;                                              %indice cicli
+    e = qB - qA;                    %errore                                        
+    de = -dq;                       %derivata errore
+    run_loop = 1;                   %condizione ciclo
+    i = 0;                          %indice cicli
 
     while run_loop && i < nmax
         i = i+1;
 
         % Dinamica diretta:
-        tau = kp.*e + kd.*de;                                            %ingresso di controllo
-        [B, C, g] = get_dynamics(q,dq,params);                           %vettore contenente [B,C,g]
+        %ingresso di controllo:
+        tau = kp.*e + kd.*de;       
+        %vettore contenente [B,C,g]:
+        [B, C, g] = get_dynamics(q,dq,params);                           
         n = C*dq + g;
-        torque_control = computed_torque_control(dq,tau,B, C, g);        %vettore coppia di controllo
+        %vettore coppia di controllo:
+        torque_control = computed_torque_control(dq,tau,B, C, g);       
 
         % Step di integrazione:
         ddq = pinv(B)*(torque_control-n);
@@ -90,7 +101,7 @@ for pos = 1:npos
 
         % Condizione sulla presenza dell'ostacolo:
         dist = sqrt((q(1) - c_o(1))^2 + (q(2) - c_o(2))^2);
-        if dist > r && z ==0
+        if dist > r && z == 0
             q_o = q;
         else
             z = 1;
@@ -101,11 +112,25 @@ for pos = 1:npos
             run_loop = 0;
         end
     end
-    
+
     % Plot posizionamento robot simulato:
     hold on
     scatter(q(1),q(2),100,'.')
-    pause(1)
+%   pause(1)
+    
+    if z == 1
+        pB = punti(7,:)';               
+        vett = [pB(1)-q_o(1); qB(2) - q_o(2)];
+        vettn = [-vett(2); vett(1)];
+        u_vettn = vettn/norm(vettn);
+        q_o1 = q_o + 0.01*u_vettn;
+        dist = sqrt((q_o1(1) - c_o(1))^2 + (q_o1(2) - c_o(2))^2);
+        if dist < r
+           q_o1 = q_o - 0.01*u_vettn;
+        end
+        punti(pos+1,:) = q_o1';
+        punti(pos+2,:) = pB';
+    end
 
     % Memorizzo posizionamenti:
     Mm(pos,1:2) = q_o;
@@ -147,17 +172,18 @@ for pos = 1:npos
 %             n = C*dq_f + g;
 %             torque_control = computed_torque_control(dq_f,tau,B, C, g);        %vettore coppia di controllo
 %                 
-%             % Control Barrier Function
+%             % Control Barrier Function:
+%             %definizione componenti per il quadprog:
 %             invB = pinv(B);
 %             H = 2*eye(2);
 %             f = - 2 * computed_torque_control(dq_f,tau,B, C, g)';
 %             A = zeros(cl-1,2);
 %             b = zeros(cl-1,1);
-%             
+%             %parametri dell'equazione di vincolo:
 %             alpha1 = 1;      %prova con 0.1, 1, 10
 %             alpha2 = 1;
 %             gamma1 = alpha2;
-%         
+%             %ciclo per costruire le n equazioni di vincolo:
 %             for ii = 1:cl-1
 %                 mi = h(ii,1:2);
 %                 bi = h(ii,3);
@@ -168,7 +194,7 @@ for pos = 1:npos
 %                     b(ii) = - b(ii);
 %                 end
 %             end
-% 
+%             %nuovo ingresso di controllo:
 %             u = quadprog(H, f, A, b, ...
 %                 [], [], [], [], [], optimoptions('quadprog','Display','off'));
 % 
@@ -186,13 +212,15 @@ for pos = 1:npos
 %                 run_loop = 0;
 %             end
 %        end
-          hold on
-         plot(x(c),y(c))
-          pause(1)
-%          scatter(q_f(1),q_f(2),'*')
-%          pause(1)
-          hold off
-
-    end
-end
-
+%         hold on
+%         plot(x(c),y(c))
+% %        pause(1)
+%         scatter(q_f(1),q_f(2),'*')
+% %        pause(1)
+%         hold off
+% 
+%     end
+% end
+hold on
+        plot(x(c),y(c))
+scatter(q_f(1),q_f(2),'*')
